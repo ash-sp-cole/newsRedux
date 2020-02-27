87adcb8036ccb46f82217186712967fce413bf00
ash-sp-cole <ash_sp_cole@hotmail.co.uk>
2020-02-27 21:06:34 +0100

    half working

--- src/apiCall.js
+++ src/apiCall.js
@@ -2,13 +2,16 @@
 import axios from 'axios';
 
  export const apiCall = () =>{
-    var url = 'http://newsapi.org/v2/top-headlines?' +
-    'sources=bbc-news&' +
-    process.env.REACT_APP_API_AUTH;
-console.log(process.env.REACT_APP_API_AUTH, "api key")
-axios.get(url)
+    var url = "http://newsapi.org/v2/top-headlines?country=us&apiKey="+ process.env.REACT_APP_API_AUTH;
+
+
+return axios.get(url)
 .then(function(response) {
-  console.log(response.json());
+  console.log(response.data.articles)
+  const data = response.data.articles
+  this.setState({
+    data
+  })
 })
 
 
--- src/components/Display/index.js
+++ src/components/Display/index.js
@@ -1,26 +1,90 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './display.css';
-import {Card,Button} from 'react-bootstrap';
-import { apiCall } from '../../apiCall';
-const Display = () =>{
-    return(
-        <div className="mainStructure">
-            <h3> DISPLAY</h3>
-            <Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src="holder.js/100px180" />
-  <Card.Body>
-    <Card.Title>Card Title</Card.Title>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
-  </Card.Body>
-</Card>
+import { Carousel, Button, InputGroup, FormControl } from 'react-bootstrap';
 
-        </div>
+import axios from 'axios';
+
+class Display extends Component {
+
+
+  state = {
+    data: [],
+    description: [],
+    image: [],
+    select: 0
+  }
+
+  componentDidMount() {
+
+    var url = "http://newsapi.org/v2/top-headlines?country=us&apiKey=" + process.env.REACT_APP_API_AUTH;
+
+
+    axios.get(url)
+      .then(response => {
+
+        console.log(response.data.articles)
+        this.setState({
+          data: response.data.articles
+          
+       
+
+        })
+
+        console.log(this.state.data)
+        console.log(this.state.data[this.state.select].description)
+      })
+
+
+  }
+
+  setValue = () => {
+    if (this.state.select < 18) {
+      this.setState({
+        select: this.state.select + 1
+      })
+    }
+    else {
+      this.setState({
+        select: 1
+      })
+    }
+
+
+  }
+
+  render() {
+    return (
+      <div className="mainStructure">
+        <h3> DISPLAY</h3>
+        <InputGroup className="mb-3" >
+          <FormControl
+            placeholder="Search......"
+            aria-label="Recipient's username"
+            aria-describedby="basic-addon2"
+            id="formValue"
+          />
+          <InputGroup.Append>
+            <Button variant="outline-secondary" onClick={() => this.setValue()} >Submit</Button>
+          </InputGroup.Append>
+        </InputGroup>
+        <Carousel>
+          <Carousel.Item>
+            <img
+              className="caroImage"
+              src={this.state.data.urlToImage}
+              alt="First slide"
+            />
+            <Carousel.Caption>
+              <h3></h3>
+              <p>{this.state.data[this.state.select].title}</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+
+        </Carousel>
+
+      </div>
     )
+  }
 }
 
-apiCall();
 export default Display;
